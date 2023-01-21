9e945d6a1c11e2361c56eee7c7f150287c46068b
Miqueias S. Costa <40896649+miqdeveloper@users.noreply.github.com>
2023-01-21 14:54:17 -0300

    Add files via upload

--- install.py
+++ install.py
@@ -2,8 +2,7 @@ from os import system
 
 try:
   system("sudo pip install -U https://github.com/iqoptionapi/iqoptionapi/archive/refs/heads/master.zip")
-  system("sudo git clone -b master https://github.com/miqdeveloper/tmp_test.git")
-  system("cd tmp_test && sudo pip install -r requirements.txt")
+  system("sudo pip install -r requirements.txt")
   system("sudo python3 start_bot.py")
 except Exception as e:
     print("Error", e)
