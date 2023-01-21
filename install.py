8848ac5661d0484bb340e4ac208ab6572cd95f29
Miqueias S. Costa <40896649+miqdeveloper@users.noreply.github.com>
2023-01-21 15:39:34 -0300

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 from os import system 
 
 try:
-  system("sudo pip install -U https://github.com/iqoptionapi/iqoptionapi/archive/refs/heads/master.zip")
+  system("sudo pip install -U https://github.com/iqoptionapi/iqoptionapi/archive/refs/heads/master.zip && sudo pip uninstall websocket-client ")
   system("sudo pip install -r requirements.txt && sudo pip install websocket-client==0.56 ")
   system("sudo python start_bot.py")
 except Exception as e:
